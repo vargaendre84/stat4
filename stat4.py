c32c9dc243f67373a54075f7e286069471a389f8
vargaendre84 <50341903+vargaendre84@users.noreply.github.com>
2019-06-01 16:57:53 +0200

    new functions

--- stat4.py
+++ stat4.py
@@ -17,6 +17,7 @@ def main():
     print("This is a simple text file statistic python script." )
     print("coded for Research Methodology subject." )
     print("Made by Varga Endre FJFAKM, EKE-GTK, Business Informatics." )
+    print("Github: https://github.com/vargaendre84/stat4")
     print()
 
     print("The corpus contains these files " )
@@ -25,7 +26,17 @@ def main():
     print("Select the filename which you want to process with its number:")
     print("the minimum number you can choose is: 1")
     print("the maximum number you can choose is: ",len(newcorpus.fileids()))
-    index = int(input('Type a number of your choice: 1 for 1st text, 2 for 2nd, ...'),10)
+    
+    while True:
+      try:
+        index = int(input('Type a number of your choice: 1 for 1st text, 2 for 2nd, ...'),10)
+      except ValueError:
+        print("Wrong input")
+        continue
+      if index < 0 or index > len(newcorpus.fileids())+1 : print("Wrong input")
+      else: break
+    #if index > len(newcorpus.fileids()) : index = len(newcorpus.fileids())
+    
     index -= 1
     print()
     print()
@@ -42,29 +53,43 @@ def main():
     
     
     
-    print("The number of tokens: ",len(wtokens))     # Number of words in text
+    print("The number of tokens: ",len(wtokens))     
     wfreq = nltk.FreqDist(wtokens)
     print()
     
     userword = input('Type the word you want to know of its frequency:')
     print()
     print("The frequency of the word '%s' : " %userword,wfreq[userword])
-    print("Number of unique words in text",len(wfreq) )     # Number of unique words in text
+    print("Number of unique words in text",len(wfreq) )    
     print()
-    print("Add how many common words you want to list! ")
-    usernumber = int(input("Type only number!"),10)
+    
+    while True:
+      try:
+        usernumber = int(input('Add how many common words you want to list!'),10)
+      except ValueError:
+        print("Wrong input")
+        continue
+      else: break
+    
     print("The %d most common words: " %usernumber,wfreq.most_common(usernumber))
 
     print()
-    sentcount = wfreq['.'] + wfreq['?'] + wfreq['!']  # Assuming every sentence ends with ., ! or ?
+    sentcount = wfreq['.'] + wfreq['?'] + wfreq['!']  
     print("The number of sentences: ",sentcount)
-    print("Average sentence length in number of words: ",len(wtokens)/sentcount )    # Average sentence length in number of words
+    print("Average sentence length in number of words: ",len(wtokens)/sentcount )    
     
     print()
-    print("Add the minimum length of words you want to list!")
-    usernumber = int(input("Type only number!"),10)
+
+    while True:
+      try:
+        usernumber = int(input('Add the minimum length of words you want to list!'),10)
+      except ValueError:
+        print("Wrong input")
+        continue
+      else: break
+    
     print("The minimum %d character long words: " % usernumber)
-    [w for w in wfreq if len(w) >= usernumber]       # all long character words
+    [w for w in wfreq if len(w) >= usernumber]       
     long = [w for w in wfreq if len(w) >= usernumber] 
     for w in long :
         print(w +" Length: %d" %len(w) +" Count: %d " %wfreq[w])  
@@ -75,13 +100,3 @@ if __name__ == "__main__":
     main()
     
     
-    
-#internet sources used for this program:
-#https://www.learnpython.org/en/Variables_and_Types
-#https://www.w3schools.com/python/python_file_open.asp
-#https://stackoverflow.com/questions/20449427/how-can-i-read-inputs-as-numbers
-#https://ncona.com/2012/02/python-lesson-getting-user-input-through-the-console-bonus-handling-an-exception/
-#http://www.pitt.edu/~naraehan/presentation/Text_Processing_Workshop.html
-#https://stackoverflow.com/questions/4951751/creating-a-new-corpus-with-nltk#
-#https://www.nltk.org/book/
-#http://users.itk.ppke.hu/~sikbo/nltk/index.php
